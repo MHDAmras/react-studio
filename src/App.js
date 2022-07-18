4808eef77a39bbec095fadc35ce53e06e58df585
MHDAmras <mhdamras13@gmail.com>
2022-07-17 18:22:33 -0700

    add file

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import react from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import Response from './components/Response';
+import Rentcard from './components/Rentcard';
+
+
+
+
+function App() {
+
+
+  return (
+   <>
+   <Header/>
+   <Home/>
+   <Response/>
+   <Rentcard/>
+   
+   </>
+    
+
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './css/style.css';
+
+const Home = () => {
+  return (
+    <header className="masthead">
+    <div className="container">
+        <div className="masthead-subheading">Welcome To Our Studio!</div>
+        <div className="masthead-heading text-uppercase">It's Nice To Meet You</div>
+        <a className="btn btn-warning btn-xl text-uppercase btss" href="#services">Tell Me More</a>
+    </div>
+</header>
+
+  )
+}
+
+export default Home;
